68638e8eb49c50aeac49bb190c4b078005dfaeee
Alberto Garcia Sola <agarciaso@agbar.ga.local>
2019-04-17 14:54:47 +0200

    ya obtengo los conciertos de la semana en el home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,37 @@
 <template>
     <div>
-        home
+        <p class="subheading">clasicaguitarra.com</p>
+        <div class="conciertosSemana">
+
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-    name: 'home'
+    name: 'home',
+    data() {
+        return {
+            conciertosWeek: []
+        }
+    },
+    mounted() {
+        this.getConciertosThisWeek()
+    },
+    methods: {
+        getConciertosThisWeek() {
+            axios.get(`${ this.$store.state.urlBackend }/conciertos/this/week`)
+                .then((res) => {
+                    console.log(res)
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        }
+    }
+
 }
 </script>
 
